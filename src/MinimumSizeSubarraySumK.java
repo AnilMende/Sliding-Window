d67593640b284b93127baa036337b67904e44ed9
Anil <anilmende2002@email.com>
2026-01-14 00:53:05 +0530

    Minimum size subarray of sum k

---
+++ src/MinimumSizeSubarraySumK.java
@@ -0,0 +1,28 @@
+public class MinimumSizeSubarraySumK {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,2,4,3};
+        System.out.println(minSubArrayLen(nums,7));
+    }
+
+    static int minSubArrayLen(int nums[] , int target){
+
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int sum = 0;
+
+        for(int right = 0;right < nums.length;right++){
+            sum += nums[right];
+
+            while(sum >= target){
+                minLen = Math.min(minLen,right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        if(minLen == Integer.MAX_VALUE){
+            return 0;
+        }
+        return minLen;
+    }
+}
