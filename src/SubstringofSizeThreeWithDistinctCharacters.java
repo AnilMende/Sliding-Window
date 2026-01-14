bf8d3497a08960ba8c402b395859244b542a9968
Anil <anilmende2002@email.com>
2026-01-14 20:04:34 +0530

    Count Good Subarray with Distinct Characers

--- src/SubstringofSizeThreeWithDistinctCharacters.java
+++ src/SubstringofSizeThreeWithDistinctCharacters.java
@@ -1,15 +1,18 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class SubstringofSizeThreeWithDistinctCharacters {
     public static void main(String[] args) {
         String s = "xyzzyxxx";
         System.out.println(countGoodSubstring(s));
+        System.out.println(countGood(s));
     }
     //a substring is called as Good Substring
     //if the substring contains all the distinct characters
     //and the length of the substring in this case is 3
     //so it is a fixed size window problem
 
+    //Using HashSet
     static int countGoodSubstring(String s){
         int count = 0;
         int left = 0;
@@ -36,6 +39,31 @@ public class SubstringofSizeThreeWithDistinctCharacters {
                     left++;
                 }
 
+            }
+        }
+        return count;
+    }
+
+    //Using HashMap
+    static int countGood(String s){
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int count = 0;
+        int left = 0;
+        for(int right = 0;right < s.length();right++){
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+
+            if(right - left + 1 == 3){
+                if(map.size() == 3){
+                    count++;
+                }
+                map.put(s.charAt(left), map.get(s.charAt(left))-1);
+                if(map.get(s.charAt(left)) == 0){
+                    map.remove(s.charAt(left));
+                }
+                left++;
             }
         }
         return count;
